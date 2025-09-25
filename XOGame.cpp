25c1dd68a52ef17af8dc48f361e7d377e75a3353
mohamed-aliii <moghonam08@gmail.com>
2025-09-25 19:22:25 +0300

    Board class

--- XOGame.cpp
+++ XOGame.cpp
@@ -10,6 +10,7 @@ enum Difficulty
   Hard
 };
 
+
 class Board
 {
 private:
@@ -19,48 +20,125 @@ private:
 public:
     Board(int s = 3) : size(s)
     {
-        grid = vector<vector<char>>(size, vector<char>(size));
+        grid = vector<vector<char>>(size, vector<char>(size, ' '));
     }
+
     void display() const
     {
-        // function implementation
+        cout << "\n  ";
+        for (int i = 0; i < size; i++) {
+            cout << "  " << i << " ";
+        }
+        cout << "\n";
+        
+        for (int i = 0; i < size; i++) {
+            cout << i << " ";
+            for (int j = 0; j < size; j++) {
+                cout << "| " << grid[i][j] << " ";
+            }
+            cout << "|\n";
+            
+            if (i < size - 1) {
+                cout << "  ";
+                for (int j = 0; j < size; j++) {
+                    cout << "+---";
+                }
+                cout << "+\n";
+            }
+        }
+        cout << "\n";
     }
+
     bool makeMove(int row, int col, char symbol)
     {
-        // function implementation
-
-        return false; // a Temporary return
+        if (isValidMove(row, col)) {
+            grid[row][col] = symbol;
+            return true;
+        }
+        return false;
     }
+
     bool isValidMove(int row, int col) const
     {
-        // function implementation
-
-        return false; // a Temporary return
+        return row >= 0 && row < size && col >= 0 && col < size && grid[row][col] == ' ';
     }
+
     bool checkWin(char symbol) const
     {
-        // function implementation
+        // Check rows
+        for (int i = 0; i < size; i++) {
+            bool rowWin = true;
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] != symbol) {
+                    rowWin = false;
+                    break;
+                }
+            }
+            if (rowWin) return true;
+        }
 
-        return false; // a Temporary return
+        // Check columns
+        for (int j = 0; j < size; j++) {
+            bool colWin = true;
+            for (int i = 0; i < size; i++) {
+                if (grid[i][j] != symbol) {
+                    colWin = false;
+                    break;
+                }
+            }
+            if (colWin) return true;
+        }
+
+        // Check main diagonal
+        bool mainDiag = true;
+        for (int i = 0; i < size; i++) {
+            if (grid[i][i] != symbol) {
+                mainDiag = false;
+                break;
+            }
+        }
+        if (mainDiag) return true;
+
+        // Check anti-diagonal
+        bool antiDiag = true;
+        for (int i = 0; i < size; i++) {
+            if (grid[i][size - 1 - i] != symbol) {
+                antiDiag = false;
+                break;
+            }
+        }
+        return antiDiag;
     }
+
     bool isFull() const
     {
-        // function implementation
-
-        return false; // a Temporary return
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
     char getCell(int row, int col) const
     {
         return grid[row][col];
     }
+
     int getSize() const
     {
-        return grid.size();
+        return size;
     }
 
     void reset()
     {
-        // function implementation
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = ' ';
+            }
+        }
     }
 };
 
